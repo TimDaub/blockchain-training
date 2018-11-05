dd406fafe862dc0703faca0b71e68d6e58d64b5f
vrde <agranzot@gmail.com>
2018-11-05 15:41:11 +0100

    Add ui, fix simple contract

--- truffle.js
+++ truffle.js
@@ -1,21 +1,21 @@
+require('dotenv').config();
 const Web3 = require("web3");
 const web3 = new Web3();
 const WalletProvider = require("truffle-wallet-provider");
 const Wallet = require('ethereumjs-wallet');
+const privateKey = process.env["RINKEBY_PRIVATE_KEY"]
+var rinkebyProvider
 
-var rinkebyPrivateKey = new Buffer(process.env["RINKEBY_PRIVATE_KEY"] || "8175D3294A31101F78931246AFC2BCBA597C922AB0615CD508DB4719B42B76E9", "hex")
-var rinkebyWallet = Wallet.fromPrivateKey(rinkebyPrivateKey);
-var rinkebyProvider = new WalletProvider(rinkebyWallet, "https://rinkeby.infura.io/");
+if (privateKey) {
+  let rinkebyPrivateKey = new Buffer(privateKey, "hex")
+  let rinkebyWallet = Wallet.fromPrivateKey(rinkebyPrivateKey);
+  rinkebyProvider = new WalletProvider(rinkebyWallet, "https://rinkeby.infura.io/");
+}
 
 
 module.exports = {
   networks: {
     development: {
-      host: "localhost",
-      port: 8545,
-      network_id: "*" // Match any network id
-    },
-    rinkeby: {
       provider: rinkebyProvider,
       // You can get the current gasLimit by running
       // truffle deploy --network rinkeby
